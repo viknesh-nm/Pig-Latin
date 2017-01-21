0c0ce2eeb6001443dfbcee393f9f84e08cb680c0
viknesh-nm <viknesh.moorthi@gmail.com>
2017-01-21 12:06:54 +0530

    Remove regex and update the code

--- pig_latin.go
+++ pig_latin.go
@@ -1,31 +1,53 @@
 package igpay
 
-import (
-	"log"
-	"regexp"
-	"strings"
-)
+import "strings"
 
-var (
-	vowels = regexp.MustCompile("^[aeiou]|^[yx][^aeiou]")
-	cons   = regexp.MustCompile(".?(ch|qu|th)[^aeiou]?|.")
-)
-
-// PigLatin translates a string into Pig Latin
-func PigLatin(text string) string {
-	words := strings.Split(text, " ")
-	pigLatin := []string{}
-	for _, word := range words {
-		pigLatin = append(pigLatin, pigLatinWord(word))
-		log.Println(pigLatin)
+// CompareString compares with the input value string and collection strings
+func CompareString(value string, collection []string) bool {
+	for _, r := range collection {
+		if r == value {
+			return true
+		}
 	}
-	return strings.Join(pigLatin, " ")
+	return false
 }
 
-func pigLatinWord(word string) string {
-	if vowels.MatchString(word) {
-		return word + "ay"
+// PigLatin translates english words into the piglatin format
+func PigLatin(text string) string {
+	words := strings.Split(text, " ")
+	var (
+		pLatinWord string
+		consField  string
+		temp       string
+	)
+	for _, word := range words {
+		consField = ""
+		if CompareString(string(word[0]), []string{"a", "e", "i", "o", "u", "y", "x"}) {
+			if string(word[1]) == "e" {
+				consField = consField + string(word[0])
+				suffix := word[len(consField):]
+				pLatinWord = suffix + consField + "ay"
+				return pLatinWord
+			}
+			pLatinWord = word + "ay"
+			return pLatinWord
+		}
+		for i, letter := range word {
+			if !CompareString(string(letter), []string{"a", "e", "i", "o"}) {
+				if string(letter) != "u" {
+					consField = consField + string(letter)
+				} else if CompareString(string(word[i+1]), []string{"a", "e", "i", "o"}) {
+					consField = consField + string(letter)
+				} else {
+					break
+				}
+			} else {
+				break
+			}
+		}
+		suffix := word[len(consField):]
+		temp = suffix + consField + "ay" + " "
+		pLatinWord = pLatinWord + temp
 	}
-	wordCount := cons.FindStringIndex(word)
-	return word[wordCount[1]:] + word[wordCount[0]:wordCount[1]] + "ay"
+	return strings.TrimSpace(pLatinWord)
 }
